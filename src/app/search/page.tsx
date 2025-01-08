c9c98c5e9975a6285c8810ac9c2359e811d10863
hungpm671 <hungpm671@gmail.com>
2025-01-09 00:51:38 +0700

    update search debuf x3

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,11 +1,14 @@
 import FreshProductSearch from "@/components/fresh_basket-search/FreshProductSearch";
 import { Box } from "@chakra-ui/react";
 import React from "react";
+import { Suspense } from "react";
 
 export default function Search() {
   return (
     <Box className="mx-auto max-w-7xl" mt={"64px"} mb={50}>
-      <FreshProductSearch />
+      <Suspense>
+        <FreshProductSearch />
+      </Suspense>
     </Box>
   );
 }
